c4247de9061782620a43d71f4cb3d825abd7e0d9
Laurens Krom <laurens.krom@gmail.com>
2020-03-30 21:22:12 +0200

    completed helloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+let talkback = ""
+if (num < 0) {
+return "ERROR" }
+for (i = 1; i <= num; i++) {
+	talkback += string}
+return talkback
 }
+
 
 module.exports = repeatString
