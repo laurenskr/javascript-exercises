492e5026d6bdd3fb668c5c570ccd45890ec31f9f
Laurens Krom <laurens.krom@gmail.com>
2020-03-30 22:08:48 +0200

    completed removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, unwanted) {
+    let args = Array.from(arguments);
+    for (i = 1; i < arguments.length; i++) {
+        const index = array.indexOf(args[i]);
+        if (index == -1) {
+            continue
+        }
+        array.splice(index, 1);
+    };
+    return array;
 }
 
 module.exports = removeFromArray
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(ltrString) {
+    reversedString =  ''
+    for (i = ltrString.length -1; i >= 0; i--) {
+        reversedString += ltrString[i]
+    };
+    return reversedString;
 }
 
 module.exports = reverseString
