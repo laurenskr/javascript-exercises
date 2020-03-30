b0654d94f00f751bd429168c0a02db474a4f6c7b
Laurens Krom <laurens.krom@gmail.com>
2020-03-30 22:51:03 +0200

    completed sumAll. Ardious, but ultimately rewarding...

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    let cijfers = Array.from(arguments).sort();
+    for (i = cijfers[0]; i <= cijfers[1]; i++) {
+        sum += i;
+    };
+    if ( typeof(a) != "number" || typeof(b) != "number" ) {
+        return "ERROR"
+    }
+    else if ( a >= 0 && b >= 0){
+        return sum
+    }
+    else {
+        return "ERROR"
+    }
 }
+    
 
 module.exports = sumAll
