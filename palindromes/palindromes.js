a327502036314fbe5ddfb7fd50b2a035a51e21b4
Laurens Krom <laurens.krom@gmail.com>
2020-04-13 21:13:44 +0200

    finished palindrome checker

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
-
+const palindromes = function(word) {
+    word = word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"")
+    let splitString = word.split("")
+    let reversedArray = splitString.reverse();
+    let joinArray = reversedArray.join("")
+    if (joinArray === word){
+        return true
+    }
+    return false
 }
-
 module.exports = palindromes
