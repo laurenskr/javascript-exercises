f1d0b87735e44b904afc6c102258cc0a1ae13eb1
Laurens Krom <laurens.krom@gmail.com>
2020-04-13 23:09:21 +0200

    code cleanup

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -5,9 +5,7 @@ const caesar = function(str, shift) {
         strChar = str.charCodeAt(i)
         if (strChar >= 65 && strChar <= 90) {
             let charNum = (strChar % 65)
-            // console.log("charNum:" + charNum)
             let shiftNum = (shift % 26)
-            // console.log("shiftNum:" + shiftNum)
             let shiftedStr = ( (charNum + shiftNum) % 26) + 65;
             if (shiftedStr < 65){
                 shiftedStr += 26
@@ -24,22 +22,4 @@ const caesar = function(str, shift) {
 }
 module.exports = caesar
 
-
-//strChar >= 60 && strChar <= 96
-//shiftedStr > 90
-//const caesar = function(str, shift) {
-//     let shiftStr = "";
-//     for (let i=0; i<str.length; i++){
-//         strChar = str.charCodeAt(i)
-//         if ((strChar >= 60 && strChar <= 90) || (strChar >= 97 && strChar <= 122 )){ //checks if char "isalpha"
-//             let shiftedStr = strChar + shift;
-//             if (1==2 || shiftedStr > 122) {
-//                 shiftedStr -= 26
-//             }
-//             shiftStr += String.fromCharCode(shiftedStr)
-//         } else {
-//             shiftStr += str[i]
-//         } 
-//     } return shiftStr
-// }
 
