913acedc218c6b683439facf0d8fd2ec9ffb3db2
Laurens Krom <laurens.krom@gmail.com>
2020-04-14 03:09:29 +0200

    completed fibonacci exercise in five minutes!

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+    let fibArray = [1,1];
+    if (Number(fibNum) <=0){
+        return "OOPS"
+    }
+    for (let i=1; i < Number(fibNum); i++){
+        fibArray.push(fibArray[i-1]+fibArray[i])
 
+    }return fibArray[Number(fibNum)-1]
 }
 
 module.exports = fibonacci
