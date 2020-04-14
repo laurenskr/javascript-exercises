23486dfcae032d46ed4061e37a56401d42a0cc7b
Laurens Krom <laurens.krom@gmail.com>
2020-04-14 03:45:32 +0200

    finished getTheOldest and finished Javascript fundamentals 5 on theOdinProject

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+   const oldest = people.sort(function(a,b) {
+    if (!b.yearOfDeath){
+        b.yearOfDeath = new Date().getFullYear()
+    }
+    const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    if (lastGuy > nextGuy){
+        return -1
+    } else {return 1}
+   }); return oldest[0]
 }
-
 module.exports = findTheOldest
