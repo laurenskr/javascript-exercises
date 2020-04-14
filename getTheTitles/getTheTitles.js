a2d9a9be5ee0665f9250e5b73f5cac5b9755fb31
Laurens Krom <laurens.krom@gmail.com>
2020-04-14 03:14:04 +0200

    completed getTheTitles!

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titleArray = []
+    for (i=0; i< arr.length; i++){
+        titleArray.push(arr[i]["title"])
+    } return titleArray
 }
 
 module.exports = getTheTitles;
