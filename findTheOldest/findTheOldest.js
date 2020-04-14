d038b5da8aadc23ece3e84e3ecf393c8eed3d3b1
Laurens Krom <laurens.krom@gmail.com>
2020-04-14 03:47:07 +0200

    added ternary operator

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -5,9 +5,10 @@ let findTheOldest = function(people) {
     }
     const lastGuy = a.yearOfDeath - a.yearOfBirth;
     const nextGuy = b.yearOfDeath - b.yearOfBirth;
-    if (lastGuy > nextGuy){
-        return -1
-    } else {return 1}
+    return lastGuy > nextGuy ? -1 : 1
+    // if (lastGuy > nextGuy){
+    //     return -1
+    // } else {return 1}
    }); return oldest[0]
 }
 module.exports = findTheOldest
